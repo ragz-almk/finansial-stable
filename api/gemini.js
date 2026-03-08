eb92410d030d40257711c4e9c317f7a4211cccbe
Raga AL Malik <ragaalmalik@gmail.com>
2026-03-08 18:53:36 +0700

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     };
 
     // Memanggil API Google Gemini
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${apiKey}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
